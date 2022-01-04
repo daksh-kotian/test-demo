a9a2fc9630ac4cacdfcdfeb4bd0789c8d56cc7a3
daksh-kotian <dakshkotian83@gmail.com>
2022-01-04 17:28:45 +0530

    test1

---
+++ chapter2/src/main/java/com/example/chapter2/funPract.java
@@ -0,0 +1,12 @@
+package com.example.chapter2;
+
+public class funPract {
+    public static void main(String[] args) {
+        int a = 6;
+        int j;
+        for(int i = 0; i<10; i++) {
+            j = i+1;
+            System.out.println(a + "X" + j + "=" + a*j);
+        }
+    }
+}
